c463f614afd9d2c3ab1b169ff18cd5b2c600e019
Ayzax <Ayzax@Outlook.com>
2025-07-06 18:45:59 +1000

    Update to v1.0.1, fix item prompts not affected by this mod

--- UltrawideHUDPlugin.cs
+++ UltrawideHUDPlugin.cs
@@ -9,26 +9,36 @@ using SettingsExtender;
 namespace Ayzax.UltrawideHUD;
 
 [BepInProcess("PEAK.exe")]
-[BepInPlugin("Ayzax.UltrawideHUD", "Ultrawide HUD", "1.0.0")]
+[BepInPlugin("Ayzax.UltrawideHUD", "Ultrawide HUD", "1.0.1")]
 [BepInDependency("SettingsExtender")]
 public class UltrawideHUDPlugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
 
     bool _active = false;
+
     RectTransform _barGroup;
     RectTransform _inventory;
+    RectTransform _prompts;
 
     UltrawideAnchorSetting _anchorSetting;
     UltrawideOffsetSetting _squishSetting;
 
     public const string SettingsCategory = "HUD";
+
+    const float HalfScreenWidth = 1920 / 2;
+
     const float BarsDefaultX = 70;
-    const float BarsFixedX = -890;
+    const float BarsFixedX = BarsDefaultX - HalfScreenWidth;
     const float BarsSquish = 250;
+
     const float InventoryDefaultX = 0;
-    const float InventoryFixedX = 960;
+    const float InventoryFixedX = HalfScreenWidth;
     const float InventorySquish = 490;
+
+    const float PromptDefaultX = -52.7703f;
+    const float PromptFixedX = HalfScreenWidth + PromptDefaultX;
+    const float PromptSquish = 490;
 
     void Start()
     {
@@ -59,6 +69,8 @@ public class UltrawideHUDPlugin : BaseUnityPlugin
 
         _barGroup = GameObject.Find("/GAME/GUIManager/Canvas_HUD/BarGroup").GetComponent<RectTransform>();
         _inventory = GameObject.Find("/GAME/GUIManager/Canvas_HUD/Inventory").GetComponent<RectTransform>();
+        _prompts = GameObject.Find("/GAME/GUIManager/Canvas_HUD/Prompts/ItemPromptLayout").GetComponent<RectTransform>();
+        _prompts.SetParent(_prompts.parent.parent);
 
         AdjustUltrawideHUD();
     }
@@ -76,17 +88,23 @@ public class UltrawideHUDPlugin : BaseUnityPlugin
         }
 
         float anchorPercent = _anchorSetting.Value;
+        float rightAnchor = 0.5f + ((1 - anchorPercent) * 0.5f);
+        float leftAnchor = anchorPercent * 0.5f;
+
         float hudSquish = _squishSetting.Value;
 
-        float barAnchor = anchorPercent * 0.5f;
-        _barGroup.anchorMin = new Vector2(barAnchor, 0);
-        _barGroup.anchorMax = new Vector2(barAnchor, 0);
+
+        _barGroup.anchorMin = new Vector2(leftAnchor, 0);
+        _barGroup.anchorMax = new Vector2(leftAnchor, 0);
         _barGroup.anchoredPosition = new Vector2(Mathf.Lerp(BarsDefaultX, BarsFixedX, anchorPercent) + (hudSquish * BarsSquish), _barGroup.anchoredPosition.y);
 
-        float inventoryAnchor = 0.5f + ((1 - anchorPercent) * 0.5f);
-        _inventory.anchorMin = new Vector2(inventoryAnchor, 0);
-        _inventory.anchorMax = new Vector2(inventoryAnchor, 0);
+        _inventory.anchorMin = new Vector2(rightAnchor, 0);
+        _inventory.anchorMax = new Vector2(rightAnchor, 0);
         _inventory.anchoredPosition = new Vector2(Mathf.Lerp(InventoryDefaultX, InventoryFixedX, anchorPercent) - (hudSquish * InventorySquish), _inventory.anchoredPosition.y);
+
+        _prompts.anchorMin = new Vector2(rightAnchor, 0);
+        _prompts.anchorMax = new Vector2(rightAnchor, 1);
+        _prompts.anchoredPosition = new Vector2(Mathf.Lerp(PromptDefaultX, PromptFixedX, anchorPercent) - (hudSquish * PromptSquish), _prompts.anchoredPosition.y);
     }
 }
 
--- metadata/CHANGELOG.md
+++ metadata/CHANGELOG.md
@@ -1,4 +1,7 @@
 # Changelog
 
+## Version 1.0.1
+- Fix item prompts not being affected by hud adjustments.
+
 ## Version 1.0.0
 - Initial release.
